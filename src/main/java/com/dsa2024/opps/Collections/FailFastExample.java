376dbf6c98a99852153c4b5aa26eb36f1fa0bf19
Bhanu <bhanu.pratap@hashstudioz.com>
2025-02-11 11:58:04 +0530

    Fail-Fast and Fail-Safe

--- src/main/java/com/dsa2024/opps/Collections/FailFastExample.java
+++ src/main/java/com/dsa2024/opps/Collections/FailFastExample.java
@@ -4,6 +4,9 @@ import java.util.ArrayList;
 import java.util.Iterator;
 
 public class FailFastExample {
+    /* Fail-Fast: Use when modification during iteration is an error.
+     * Single-threaded iteration where modifications should be caught.
+    */
     public static void main(String[] args) {
         ArrayList<String> list = new ArrayList<>();
         list.add("A");
--- src/main/java/com/dsa2024/opps/Collections/FailSafeExample.java
+++ src/main/java/com/dsa2024/opps/Collections/FailSafeExample.java
@@ -3,6 +3,9 @@ package com.dsa2024.opps.Collections;
 import java.util.concurrent.CopyOnWriteArrayList;
 
 public class FailSafeExample {
+    /*Fail-Safe: Use for concurrent access without risking exceptions.
+     * Multi-threaded environments where modifications are expected.
+    */
     public static void main(String[] args) {
         CopyOnWriteArrayList<String> list = new CopyOnWriteArrayList<>();
         list.add("A");
