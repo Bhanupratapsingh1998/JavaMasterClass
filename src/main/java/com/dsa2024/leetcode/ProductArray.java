f9cd8e063aa7bc4300f0b0f0bccbd13cb45d28bf
Bhanu <bhanu.pratap@hashstudioz.com>
2025-04-16 10:23:29 +0530

    Product array optimized

--- src/main/java/com/dsa2024/leetcode/ProductArray.java
+++ src/main/java/com/dsa2024/leetcode/ProductArray.java
@@ -4,25 +4,22 @@ import java.util.Arrays;
 
 public class ProductArray {
     private static int[] productArray(int[] numbers, int n) {
+        int[] result = new int[n];
 
-        int[] left = new int[n];
-        int[] right = new int[n];
-        int[] output = new int[n];
-
-        left[0] = 1;
+        // Step 1: Calculate prefix product for each index
+        result[0] = 1;
         for (int i = 1; i < n; i++) {
-            left[i] = left[i - 1] * numbers[i - 1];
+            result[i] = result[i - 1] * numbers[i - 1];
         }
 
-        right[n - 1] = 1;
-        for (int i = n - 2; i >= 0; i--) {
-            right[i] = right[i + 1] * numbers[i + 1];
+        // Step 2: Multiply with suffix product  1,1,2,6
+        int suffixProduct = 1;
+        for (int i = n - 1; i >= 0; i--) {
+            result[i] = result[i] * suffixProduct;
+            suffixProduct = suffixProduct * numbers[i];
         }
 
-        for (int i = 0; i < n; i++) {
-            output[i] = left[i] * right[i];
-        }
-        return output;
+        return result;
     }
 
     public static void main(String[] args) {
