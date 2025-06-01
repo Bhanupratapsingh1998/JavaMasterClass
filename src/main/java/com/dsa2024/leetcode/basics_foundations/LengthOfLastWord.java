d94e3a5e45ec9a274858a13504094234be40b8a9
Bhanu <bhanupratapsinghq@gmail.com>
2025-06-01 19:06:27 +0530

    Length of last word

--- src/main/java/com/dsa2024/leetcode/basics_foundations/LengthOfLastWord.java
+++ src/main/java/com/dsa2024/leetcode/basics_foundations/LengthOfLastWord.java
@@ -1,19 +1,22 @@
-package com.dsa2024.leetcode;
+package com.dsa2024.leetcode.basics_foundations;
 
 public class LengthOfLastWord {
 
     // Time Complexity: O(n), where n is the length of the string.
     // Space Complexity: O(1), as we are using a constant amount of space.
     public static int lengthOfLastWord(String s) {
+        int i = s.length() - 1;
+
+        // Skip trailing spaces
+        while (i >= 0 && s.charAt(i) == ' ') {
+            i--;
+        }
+
         int count = 0;
-        for (int i = s.length() - 1; i >= 0; i--) {
-            if (count > 0 && s.charAt(i) == ' ') {
-                break;
-            } else if (s.charAt(i) == ' ') {
-                count = 0;
-            } else {
-                count++;
-            }
+        // Count characters until next space or start of string
+        while (i >= 0 && s.charAt(i) != ' ') {
+            count++;
+            i--;
         }
         return count;
     }
