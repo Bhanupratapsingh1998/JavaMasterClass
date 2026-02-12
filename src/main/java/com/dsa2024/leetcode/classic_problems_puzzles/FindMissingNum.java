aa7ac3564b1ead37a233f526cba54866a7f2e052
Bhanu <bhanu.pratap@hashstudioz.com>
2026-02-12 09:40:14 +0530

    Longest coomon substring

--- src/main/java/com/dsa2024/leetcode/classic_problems_puzzles/FindMissingNum.java
+++ src/main/java/com/dsa2024/leetcode/classic_problems_puzzles/FindMissingNum.java
@@ -1,8 +1,8 @@
 package com.dsa2024.leetcode.classic_problems_puzzles;
 
 public class FindMissingNum {
-    static int findMissingNumber(int[] arr, int N) {
-        int expectedSum = N * (N + 1) / 2;
+    static int findMissingNumber(int[] arr, int n) {
+        int expectedSum = n * (n + 1) / 2;
         int actualSum = 0;
         for (int num : arr) {
             actualSum += num;
@@ -13,6 +13,6 @@ public class FindMissingNum {
     public static void main(String[] args) {
         int arr[] = { 1, 2, 4, 5, 6 };
         int k = findMissingNumber(arr, 6);
-        System.out.println("Find the missing number is " + k);
+        System.out.println("Find the missing number is : " + k);
     }
 }
